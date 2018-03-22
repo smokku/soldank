e6b413ee86b49a252d10eeb870709a5d8f89aaef
Mariano Cuatrin <mariano.cuatrin@gmail.com>
2018-03-22 19:10:52 -0300

    use pixels_mut() instead of enumerate_pixels_mut() to iterate (coords not needed)

--- gfx2d/src/texture.rs
+++ gfx2d/src/texture.rs
@@ -65,7 +65,7 @@ pub fn load_image_rgba<P: AsRef<Path>>(filename: P) -> image::RgbaImage {
 }
 
 pub fn premultiply_image(img: &mut image::RgbaImage) {
-    for (_, _, pixel) in img.enumerate_pixels_mut() {
+    for pixel in img.pixels_mut() {
         let a = pixel[3] as f32 / 255.0;
 
         *pixel = image::Rgba([
@@ -78,7 +78,7 @@ pub fn premultiply_image(img: &mut image::RgbaImage) {
 }
 
 pub fn remove_color_key(img: &mut image::RgbaImage, color_key: Color) {
-    for (_, _, pixel) in img.enumerate_pixels_mut() {
+    for pixel in img.pixels_mut() {
         if rgba(pixel[0], pixel[1], pixel[2], pixel[3]) == color_key {
             *pixel = image::Rgba([0, 0, 0, 0]);
         }
