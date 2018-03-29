1600e9f75a1af2f89655c475c464e2b93667b8ef
helloer <im.helloer@gmail.com>
2018-03-29 20:04:17 +0200

    Revert "flattened use statements so it compiles with stable toolchain"
    This reverts commit 22c847bddae4f3d9050dba98785fd5998c4d868b.

--- gfx2d/src/gfx_types.rs
+++ gfx2d/src/gfx_types.rs
@@ -1,31 +1,47 @@
 // included from lib.rs
 // traits are "used" where needed to avoid unused warnings (compiler bug?)
 
-use glutin::GlWindow;
-use glutin::EventsLoop;
-use gfx::pso::bundle::Bundle;
-use gfx::buffer::Role::Vertex as VertexRole;
-use gfx::memory::Bind;
-use gfx::memory::Usage::Dynamic;
-use gfx::format::Rgba8;
-use gfx::format::Srgba8;
-use gfx::format::R8_G8_B8_A8;
-use gfx::format::DepthStencil;
-use gfx::format::U8Norm as U8N;
-use gfx::texture::AaMode;
-use gfx::texture::Kind::D2;
-use gfx::texture::SamplerInfo;
-use gfx::texture::Mipmap;
-use gfx::state::ColorMask;
-use gfx::state::Blend;
-use gfx::state::BlendChannel;
-use gfx::state::BlendValue;
-use gfx::state::Equation;
-use gfx::state::Factor;
-use gfx_device_gl::Resources as R;
-use gfx_device_gl::Device as GlDevice;
-use gfx_device_gl::Factory as GlFactory;
-use gfx_device_gl::CommandBuffer as GlCommandBuffer;
+use {
+    glutin::{
+        GlWindow,
+        EventsLoop,
+    },
+    gfx::{
+        pso::bundle::Bundle,
+        buffer::Role::Vertex as VertexRole,
+        memory::{
+            Bind,
+            Usage::Dynamic,
+        },
+        format::{
+            Rgba8,
+            Srgba8,
+            R8_G8_B8_A8,
+            DepthStencil,
+            U8Norm as U8N,
+        },
+        texture::{
+            AaMode,
+            Kind::D2,
+            SamplerInfo,
+            Mipmap,
+        },
+        state::{
+            ColorMask,
+            Blend,
+            BlendChannel,
+            BlendValue,
+            Equation,
+            Factor,
+        },
+    },
+    gfx_device_gl::{
+        Resources as R,
+        Device as GlDevice,
+        Factory as GlFactory,
+        CommandBuffer as GlCommandBuffer,
+    },
+};
 
 // type aliases
 
