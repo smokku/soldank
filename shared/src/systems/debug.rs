7e42ea70e461af52f2b0e4cae5e320301827852f
Tomasz Sterna <tomasz@sterna.link>
2021-03-22 14:58:24 +0100

    Replace legion with hecs in shared

--- shared/src/systems/debug.rs
+++ shared/src/systems/debug.rs
@@ -1,15 +1,12 @@
-use legion::system;
 use std::{collections::VecDeque, net::SocketAddr};
 
 use crate::{messages::NetworkMessage, systems};
 
-#[system]
-pub fn tick_debug(#[resource] time: &systems::Time) {
+pub fn tick_debug(time: &systems::Time) {
     log::debug!("tick {}", time.tick);
 }
 
-#[system]
-pub fn message_dump(#[resource] messages: &mut VecDeque<(SocketAddr, NetworkMessage)>) {
+pub fn message_dump(messages: &mut VecDeque<(SocketAddr, NetworkMessage)>) {
     for (addr, message) in messages.drain(..) {
         log::warn!("{}: {:#?}", addr, message);
     }
--- shared/src/systems/movement.rs
+++ shared/src/systems/movement.rs
@@ -1,10 +1,9 @@
-use legion::{system, world::SubWorld, Query};
+use hecs::World;
 
-use crate::components::Particle;
+use crate::components::*;
 
-#[system]
-pub fn kinetic_movement(world: &mut SubWorld, query: &mut Query<&mut Particle>) {
-    for body in query.iter_mut(world) {
+pub fn kinetic_movement(world: &mut World) {
+    for (_entity, body) in world.query::<&mut Particle>().iter() {
         if body.active {
             body.euler();
         }
